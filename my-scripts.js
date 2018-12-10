4590a92c3291f69008676b7b77457621dae3f6c4
jmossie <jaquesmostert@gmail.com>
2018-12-10 15:43:55 -0700

    Add files via upload

--- my-scripts.js
+++ my-scripts.js
@@ -2,6 +2,12 @@
 if(localStorage.getItem("count") == null) {
 	localStorage.setItem("count", 0)
 }
+for (var i = 1; i <= localStorage.getItem("count"); i++) {
+		var span = document.createElement('span');
+		span.innerHTML = '<img src="coffee.jpg">';
+		var body2 = document.getElementsByTagName('body2')[0];
+		body2.appendChild(span);
+	}
 
 //update count
 function update() {
@@ -12,8 +18,10 @@ update();
 
 //add one to count
 function plusOne() {
-	localStorage.setItem("count", (Number(localStorage.getItem("count")) + 1));
+	localStorage.setItem("count", (Number(localStorage.getItem("count")) + 1));	
+
 	update();
+	location.reload();
 }
 
 //reset count
@@ -21,5 +29,11 @@ function reset() {
 	if (confirm("Are you sure you want to reset?")) {
     	localStorage.setItem("count",0);
 		update();
+		location.reload();
 	}
 }
+
+//function addimage(){
+
+//}
+
